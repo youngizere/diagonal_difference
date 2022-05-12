e3b24c0bf689fa775668fd99e9f3c24a18cb6b48
youngizere <Young.Izere@strathmore.edu>
2022-05-12 17:01:12 +0300

    Solution refinement

--- solution.py
+++ solution.py
@@ -4,11 +4,14 @@ def diagonal_difference(input_square_matrix):
     # Initializing the right to left diagonal summation
     r_l_diag_summation = 0
     matrix_row_size = len(input_square_matrix)
+    
+    # The diagonal elements are summed together by iterating through
+    # the matrix elements which correspond to the digonal element positions [][]
 
     for item in range(0, matrix_row_size):
         l_r_diag_summation = l_r_diag_summation + input_square_matrix[item][item]
         r_l_diag_summation = r_l_diag_summation + input_square_matrix[item][matrix_row_size - item - 1]
-    
+    # The abs() function return the absolute value of a given number
     abs_diff = abs(l_r_diag_summation - r_l_diag_summation)
     return abs_diff
 
