2af65deb7093a8bd866f2ab62af81a1339d5924f
youngizere <Young.Izere@strathmore.edu>
2022-05-12 13:35:57 +0300

    Solution submission

---
+++ solution.py
@@ -0,0 +1,20 @@
+def diagonal_difference(input_square_matrix):
+    # Initializing the left to right diagonal summation
+    l_r_diag_summation = 0
+    # Initializing the right to left diagonal summation
+    r_l_diag_summation = 0
+    matrix_row_size = len(input_square_matrix)
+
+    for item in range(0, matrix_row_size):
+        l_r_diag_summation = l_r_diag_summation + input_square_matrix[item][item]
+        r_l_diag_summation = r_l_diag_summation + input_square_matrix[item][matrix_row_size - item - 1]
+    
+    abs_diff = abs(l_r_diag_summation - r_l_diag_summation)
+    return abs_diff
+
+if __name__=="__main__":
+    input_square_matrix = [ [1, 2, 3],
+                            [4, 5, 6],
+                            [9, 8, 9]]
+
+    print(diagonal_difference(input_square_matrix))
